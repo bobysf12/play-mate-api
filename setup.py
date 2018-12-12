36d00b9a404f7c020d314e2bd21bfb9f2cf39f78
maulanasly <gi.creatorz@gmail.com>
2018-12-12 18:32:52 +0700

    initial project

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='PlaymateAPI',
+    version='0.1',
+    long_description=__doc__,
+    author="Maulana Yusuf",
+    author_email="im.idiiot@gmail.com",
+    packages=['playmate', 'playmate.resources', 'playmate.models', 'playmate.helpers'],
+    include_package_data=True,
+    install_requires=[
+        'flask>=0.10.1',
+        'flask-restful',
+        'flask-restful-swagger',
+        'raven[flask]',
+        'Flask-PyMongo',
+        'pymongo',
+        'bcrypt',
+        'cloudinary',
+        'flask-FCM',
+    ],
+    dependency_links=[
+    ]
+)
